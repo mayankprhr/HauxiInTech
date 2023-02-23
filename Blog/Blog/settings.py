9361ca61e7f86540fd46f07794bb88d3741eda5e
Mayank Parihar <itsmayankprhr@gmail.com>
2023-02-24 01:37:43 +0530

    commit2

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend '
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'carl.hauxi@gmail.com'
-EMAIL_HOST_PASSWORD ='Bugatti Veyron'
+EMAIL_HOST_USER = '********@gmail.com'
+EMAIL_HOST_PASSWORD ='*****'
